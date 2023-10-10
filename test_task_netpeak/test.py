ff4186b8f46092c7fcfb6629f602e1a7b2574bea
Victoria Nesterenko <DanilDanill@gmail.com>
2023-10-10 12:05:54 +0300

    fix

--- test_task_netpeak/test.py
+++ test_task_netpeak/test.py
@@ -2,56 +2,50 @@ import sqlite3
 import os
 
 
-def database_exists(db_file):
-    return os.path.isfile(db_file)
-
-
 def create_tables_and_insert_data():
     db_file = "bonus.db"
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
 
-    if not database_exists(db_file):
-        create_users_table = """
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            department_id INTEGER,
-            salary REAL
-        );
-        """
+    create_users_table = """
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        department_id INTEGER,
+        salary REAL
+    );
+    """
 
-        create_department_table = """
-        CREATE TABLE IF NOT EXISTS department (
-            department_id INTEGER PRIMARY KEY,
-            bonus_rate REAL
-        );
-        """
+    create_department_table = """
+    CREATE TABLE IF NOT EXISTS department (
+        department_id INTEGER PRIMARY KEY,
+        bonus_rate REAL
+    );
+    """
 
-        insert_users_data = """
-        INSERT INTO users (name, department_id, salary)
-        VALUES
-            ('John', 1, 1000.0),
-            ('Alice', 2, 1200.0),
-            ('Bob', 1, 1500.0),
-            ('Carol', 3, 900.0),
-            ('David', 2, 800.0);
-        """
+    insert_users_data = """
+    INSERT INTO users (name, department_id, salary)
+    VALUES
+        ('John', 1, 1000.0),
+        ('Alice', 2, 1200.0),
+        ('Bob', 1, 1500.0),
+        ('Carol', 3, 900.0),
+        ('David', 2, 800.0);
+    """
 
-        insert_department_data = """
-        INSERT OR REPLACE INTO department (department_id, bonus_rate)
-        VALUES
-            (1, 0.1),
-            (2, 0.2),
-            (3, 0.15);
-        """
+    insert_department_data = """
+    INSERT OR REPLACE INTO department (department_id, bonus_rate)
+    VALUES
+        (1, 0.1),
+        (2, 0.2),
+        (3, 0.15);
+    """
 
-        cursor.execute(create_users_table)
-        cursor.execute(create_department_table)
-        cursor.execute(insert_users_data)
-        cursor.execute(insert_department_data)
-        conn.commit()
-
+    cursor.execute(create_users_table)
+    cursor.execute(create_department_table)
+    cursor.execute(insert_users_data)
+    cursor.execute(insert_department_data)
+    conn.commit()
     conn.close()
 
 
